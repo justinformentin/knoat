4eb0737ff164886e907fb337d89de4b86e9634c1
justinformentin <justin.formentin@gmail.com>
2019-02-18 20:58:09 -0500

    Removed css files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import configureStore from './redux/store';
-
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 

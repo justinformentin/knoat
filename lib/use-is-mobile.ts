d0c9de77c4a4ed0021b2d8d40dc9fa8218ec79cd
Justin Formentin <justin.formentin@gmail.com>
2024-11-20 18:07:11 -0500

    Improve use is mobile hook

--- lib/use-is-mobile.ts
+++ lib/use-is-mobile.ts
@@ -1,32 +1,21 @@
 import * as React from 'react';
 
-export const useIsMobile = (mobileScreenSize = 768) => {
-  if (typeof window.matchMedia !== 'function') {
-    throw Error('matchMedia not supported by browser!');
-  }
-  const [isMobile, setIsMobile] = React.useState(window.matchMedia(`(max-width: ${mobileScreenSize}px)`).matches);
+const MOBILE_BREAKPOINT = 768;
 
-  const checkIsMobile = React.useCallback((event:any) => {
-    setIsMobile(event.matches);
-  }, []);
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined,
+  );
 
   React.useEffect(() => {
-    const mediaListener = window.matchMedia(`(max-width: ${mobileScreenSize}px)`);
-    // try catch used to fallback for browser compatibility
-    try {
-      mediaListener.addEventListener('change', checkIsMobile);
-    } catch {
-      mediaListener.addListener(checkIsMobile);
-    }
-
-    return () => {
-      try {
-        mediaListener.removeEventListener('change', checkIsMobile);
-      } catch {
-        mediaListener.removeListener(checkIsMobile);
-      }
-    }
-  }, [mobileScreenSize]);
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener('change', onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener('change', onChange);
+  }, []);
 
   return isMobile;
-};
+}
